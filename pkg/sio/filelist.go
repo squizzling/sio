63bb5272165b5e17370c6e74b0ddc0d4765db845
squizzling <squizzling@users.noreply.github.com>
2020-12-22 11:57:25 +1100

    Add RecursiveGlobDirs to just return directories. Also create our own channel.

--- pkg/sio/filelist.go
+++ pkg/sio/filelist.go
@@ -13,15 +13,14 @@ import (
 //
 // The order in which files are sent to the channel is not deterministic.
 //
-// The channel is created by the caller so they can control buffering, and returned from the function
-// so the caller can use it in a range statement.
+// Buffer controls the size of the channel, so it can be buffered or not as the caller chooses.
 //
-// RecursiveGlob is responsible for closing the channel, regardless of whether or not ctx is
-// cancelled.  To prevent a goroutine leak, the caller must either cancel the context (which
-// will always clean up), or receive from the channel until it is closed.
+// To prevent a goroutine leak, the caller must either cancel the context (which will always clean up),
+// or receive from the channel until it is closed.
 //
 // Any error will trigger a panic
-func RecursiveGlob(ctx context.Context, ch chan string, patterns ...string) <-chan string {
+func RecursiveGlob(ctx context.Context, buffer int, patterns ...string) <-chan string {
+	ch:= make(chan string, buffer)
 	go func() {
 		defer close(ch)
 		ctxDone := ctx.Done()
@@ -56,6 +55,43 @@ func RecursiveGlob(ctx context.Context, ch chan string, patterns ...string) <-ch
 					// not a directory
 					// not a regular file
 					// ignore it
+				}
+			}
+		}
+	}()
+	return ch
+}
+
+// RecursiveGlobDirs is like RecursiveGlob except it only sends directories.
+//
+// Note that a pattern of /foo/* will not send /foo.  To include the base directory,
+// use a pattern of /foo.
+func RecursiveGlobDirs(ctx context.Context, buffer int, patterns ...string) <-chan string {
+	ch := make(chan string, buffer)
+	go func() {
+		defer close(ch)
+		ctxDone := ctx.Done()
+		dirStack := make([]string, 0, len(patterns))
+		for _, pattern := range patterns {
+			for _, name := range FilepathGlob(pattern) {
+				absName := FilepathAbs(name)
+				if fi := OsLstat(absName); fi.IsDir() {
+					dirStack = append(dirStack, absName)
+				}
+
+			}
+		}
+
+		for len(dirStack) > 0 {
+			dirName := dirStack[len(dirStack)-1]
+			dirStack = dirStack[:len(dirStack)-1]
+			if !sendFilename(ctxDone, ch, dirName) {
+				return
+			}
+
+			for _, childInfo := range FReadDirAll(dirName) {
+				if childInfo.IsDir() {
+					dirStack = append(dirStack, filepath.Join(dirName, childInfo.Name()))
 				}
 			}
 		}
